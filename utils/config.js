6db5301864853edd7447d9409a8b61a15e5ddb08
zhangxiang <me_zhangxiang@163.com>
2018-10-11 16:29:52 +0800

    xxx

--- utils/config.js
+++ utils/config.js
@@ -1,12 +1,7 @@
 module.exports = {
   apiHost: "http://192.168.1.88:8080/xcx/",
-<<<<<<< HEAD
   downloadInterface: "http://192.168.1.88:8080/attachment/download/",
   uploadInterface:"http://192.168.1.88:8080/attachment/upload",
-=======
-  downloadInterface: "http://192.168.1.100:8080/attachment/download/",
-  uploadInterface:"http://192.168.1.100:8080/attachment/upload",
->>>>>>> bc16ffed600c095ba1c9f0783277145603003858
   mapkey: "Z2EBZ-EXYEJ-YTMFD-K3Y4O-3HQEK-PPF5U",
   loginApi: 'index/login'
 };
