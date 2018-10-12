f3d683cba66d613bd4d93db379b32af4395cebfd
zhangxiang <me_zhangxiang@163.com>
2018-10-12 17:03:16 +0800

    zx

--- pages/me/become-distributor/distribution-center/distribution-order/distribution-order.js
+++ pages/me/become-distributor/distribution-center/distribution-order/distribution-order.js
@@ -1,84 +1,112 @@
-const http=require("../../../../../utils/http.js")
+const http = require("../../../../../utils/http.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    data:[],
+    data: [],
+    allmoney: "",
+    pageIndex: 1, //分页默认第一页
+    noMore:false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.requestOrder()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+    this.setData({
+      data:[]
+    })
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+    let index = this.data.pageIndex + 1;//每下拉一次请求页码+1
+    this.setData({
+      pageIndex: index
+    })
+    if(!this.data.noMore){
+      this.requestOrder()
+    }else{
+      console.log("没有更多")
+    }
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   },
-  requestOrder(){
+  requestOrder() {
     http.request({
       apiName: 'my/distributororder',
       method: 'POST',
-      data:{
-        'currentPage':1
+      data: {
+        'currentPage': this.data.pageIndex
       },
       success: res => {
         console.log(res)
-        // this.setData({
-        //   data: res
-        // })
+        if (res.list.length==0) {
+          this.setData({
+            noMore:true
+          })
+        }else{
+          let newArray = this.data.data.concat(res.list)
+          this.setData({
+            data: newArray
+          })
+        }
+        
+        //总佣金
+        let sum = 0;
+        for (let value of this.data.data) {
+          sum = sum + Number(value.income)
+        }
+        this.setData({
+          allmoney: sum
+        })
       },
-      fail:err=>{
-        console.log(err)
-      }
+
     })
   }
 })
