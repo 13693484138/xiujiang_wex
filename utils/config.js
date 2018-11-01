e8f685deabd1bc370ec99340083d91b5ff8a9cd7
smx <644271331@qq.com>
2018-11-01 15:42:51 +0800

    sms

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiHost: "http://192.168.1.100:8080/xcx/",
+  apiHost: "https://shop.ngrok.michaelch.xyz/xcx/",
   downloadInterface: "https://shop.ngrok.michaelch.xyz/attachment/download/",
   uploadInterface:"https://shop.ngrok.michaelch.xyz/attachment/upload",
   mapkey: "Z2EBZ-EXYEJ-YTMFD-K3Y4O-3HQEK-PPF5U",
