044a707c274a27b12ae820ecc7ec75dcf864a9f1
smx <644271331@qq.com>
2018-11-05 10:13:09 +0800

    smx

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  apiHost: "https://shop.ngrok.michaelch.xyz/xcx/",
-  downloadInterface: "https://shop.ngrok.michaelch.xyz/attachment/download/",
-  uploadInterface:"https://shop.ngrok.michaelch.xyz/attachment/upload",
+  apiHost: "https://new.xiujiangxiu.com/xcx/",
+  downloadInterface: "https://new.xiujiangxiu.com/attachment/download/",
+  uploadInterface:"https://new.xiujiangxiu.com/attachment/upload",
   mapkey: "Z2EBZ-EXYEJ-YTMFD-K3Y4O-3HQEK-PPF5U",
   loginApi: 'index/login'
 };
